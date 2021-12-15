023e5e081a98b01f5c1f2bf41988dbe53f989017
Ekkehard C. Damisch <fhs42273@fh-salzburg.ac.at>
2021-12-15 19:53:20 +0100

    trying to avoid the nvidia drivers

--- 1-setup.sh
+++ 1-setup.sh
@@ -225,12 +225,12 @@ case "$proc_type" in
 esac	
 
 # Graphics Drivers find and install
-if lspci | grep -E "NVIDIA|GeForce"; then
-    pacman -S nvidia --noconfirm --needed
-	nvidia-xconfig
-elif lspci | grep -E "Radeon"; then
-    pacman -S xf86-video-amdgpu --noconfirm --needed
-elif lspci | grep -E "Integrated Graphics Controller"; then
+# if lspci | grep -E "NVIDIA|GeForce"; then
+#     pacman -S nvidia --noconfirm --needed
+# 	nvidia-xconfig
+# elif lspci | grep -E "Radeon"; then
+#     pacman -S xf86-video-amdgpu --noconfirm --needed
+if lspci | grep -E "Integrated Graphics Controller"; then
     pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils --needed --noconfirm
 fi
 
