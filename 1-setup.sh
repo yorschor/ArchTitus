d6cf77608d2ecf22a967ad87194352fd42d9cb2e
Ekkehard C. Damisch <yorschor@gmail.com>
2021-12-16 14:15:13 +0100

    force install of Integrated Graphics Controller

--- 1-setup.sh
+++ 1-setup.sh
@@ -95,12 +95,12 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 # Graphics Drivers find and install
-if lspci | grep -E "NVIDIA|GeForce"; then
-    pacman -S nvidia --noconfirm --needed
-	nvidia-xconfig
-elif lspci | grep -E "Radeon"; then
-    pacman -S xf86-video-amdgpu --noconfirm --needed
-elif lspci | grep -E "Integrated Graphics Controller"; then
+# if lspci | grep -E "NVIDIA|GeForce"; then
+#     pacman -S nvidia --noconfirm --needed
+# 	nvidia-xconfig
+# elif lspci | grep -E "Radeon"; then
+#     pacman -S xf86-video-amdgpu --noconfirm --needed
+if lspci | grep -E "Integrated Graphics Controller"; then
     pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils --needed --noconfirm
 fi
 echo -ne "
